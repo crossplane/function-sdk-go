191462b0f41202f20c08c25cabfbcf575c130f8e
Nic Cope <nicc@rk0n.org>
2023-08-31 15:05:08 -0700

    Add generated gRPC APIs
    I'm not exactly sure how this is going to work. My gut feeling is the
    easiest thing to do will be for each SDK to have its own copy of the
    run_function.proto file, with the 'authoritative' copy being the one
    from c/c.
    
    Signed-off-by: Nic Cope <nicc@rk0n.org>

---
+++ proto/generate.go
@@ -0,0 +1,43 @@
+//go:build generate
+// +build generate
+
+/*
+Copyright 2019 The Crossplane Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// NOTE(negz): See the below link for details on what is happening here.
+// https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
+
+// Generate gRPC types and stubs.
+//
+// We use buf rather than the traditional protoc because it's pure go and can
+// thus be invoked using go run from a pinned dependency. If we used protoc we'd
+// need to install it somehow, and there are not currently statically compiled
+// binaries available for download (the release binaries for Linux are
+// dynamically linked). See buf.gen.yaml for buf's configuration.
+//
+// We go install the required plugins because they need to be in $PATH for buf
+// (or protoc) to invoke them.
+
+//go:generate go install google.golang.org/protobuf/cmd/protoc-gen-go google.golang.org/grpc/cmd/protoc-gen-go-grpc
+//go:generate go run github.com/bufbuild/buf/cmd/buf generate
+
+package proto
+
+import (
+	_ "github.com/bufbuild/buf/cmd/buf"               //nolint:typecheck
+	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc" //nolint:typecheck
+	_ "google.golang.org/protobuf/cmd/protoc-gen-go"  //nolint:typecheck
+)
