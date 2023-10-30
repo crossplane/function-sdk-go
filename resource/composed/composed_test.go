9fc84aa24dac11e3e33dc8ed1f37d6b266f670cf
Nic Cope <nicc@rk0n.org>
2023-10-29 23:32:14 -0700

    Demonstrate that MR structs marshal with extraneous fields
    Signed-off-by: Nic Cope <nicc@rk0n.org>

--- resource/composed/composed_test.go
+++ resource/composed/composed_test.go
@@ -19,6 +19,9 @@ package composed
 import (
 	"fmt"
 
+	"github.com/upbound/provider-aws/apis/s3/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/ptr"
 	"sigs.k8s.io/yaml"
 )
 
@@ -51,4 +54,43 @@ func Example() {
 	//     coolness: high
 	// spec:
 	//   coolness: 9001
+}
+
+func ExampleFrom() {
+	// Create a strongly typed runtime.Object, imported from a provider.
+	b := &v1beta1.Bucket{
+		ObjectMeta: metav1.ObjectMeta{
+			Labels: map[string]string{
+				"coolness": "high",
+			},
+		},
+		Spec: v1beta1.BucketSpec{
+			ForProvider: v1beta1.BucketParameters{
+				Region: ptr.To[string]("us-east-2"),
+			},
+		},
+	}
+
+	// Create a composed resource from the runtime.Object.
+	cd, err := From(b)
+	if err != nil {
+		panic(err)
+	}
+
+	// Marshal our composed resource to YAML. We just do this for illustration
+	// purposes. Normally you'd add it to the map of desired resources you send
+	// in your RunFunctionResponse.
+	y, _ := yaml.Marshal(cd)
+
+	fmt.Println(string(y))
+
+	// Output:
+	// apiVersion: s3.aws.upbound.io/v1beta1
+	// kind: Bucket
+	// metadata:
+	//   labels:
+	//     coolness: high
+	// spec:
+	//   forProvider:
+	//     region: us-east-2
 }
