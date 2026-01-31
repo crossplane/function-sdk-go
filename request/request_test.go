9f5da81fd88bae2b07f41f7974be57a6257915f6
Bob Haddleton <bob.haddleton@nokia.com>
2026-01-31 10:51:04 -0600

    Fix lint errors
    Signed-off-by: Bob Haddleton <bob.haddleton@nokia.com>

--- request/request_test.go
+++ request/request_test.go
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package request contains utilities for working with RunFunctionRequests.
 package request
 
 import (
